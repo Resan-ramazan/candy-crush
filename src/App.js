c5e8a10ab18dca5fb11ab69d768a2aa53b8d8a16
Ramazan Alkoyun <ramazan.resan@gmail.com>
2023-01-11 18:51:42 +0100

    Create board and check column of tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import blueCandy from "./images/blue-candy.png";
+import greenCandy from "./images/green-candy.png";
+import orangeCandy from "./images/orange-candy.png";
+import purpleCandy from "./images/purple-candy.png";
+import redCandy from "./images/red-candy.png";
+import yellowCandy from "./images/yellow-candy.png";
 
-function App() {
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const width = 8;
+
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const App = () => {
+  const [currentColorArrangment, setCurrentColorArrangment] = useState([]);
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i <= 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangment[i];
+      const isBlank = currentColorArrangment[i] === "";
+
+      if (
+        columnOfThree.every(
+          (square) =>
+            currentColorArrangment[square] === decidedColor && !isBlank
+        )
+      ) {
+        columnOfThree.forEach((square) => {
+          currentColorArrangment[square] = "";
+        });
+      }
+    }
+  };
+
+  const createBoard = () => {
+    const randomColorArrangment = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangment.push(randomColor);
+    }
+    setCurrentColorArrangment(randomColorArrangment);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setCurrentColorArrangment([...currentColorArrangment]);
+    }, 100);
+    return () => clearInterval(timer); // This is the cleanup function
+  }, [checkForColumnOfThree, currentColorArrangment]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangment.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
+        ))}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
